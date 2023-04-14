8c2ef651d99379b463b3af091e1b2978ddff7890
Tanisha <jaiswaltanisha11@gmail.com>
2023-04-15 00:57:06 +0530

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-       child: Center(
-          child: Container(
-            child: Text("Welcome to my first flutter project"),
-          ),
-       ),
-      ),
-    );
+    
+    // int days = 30;
+    // String name = "Tanisha";
+    // double pi = 3.14;
+    // bool isMale = false;
+    // num temp = 30.5; // this takes both int nad double values
+
+    // var day = "Tuesday";
+    // const pi2 = 3.14;
+    // final number = 12;
+
+    // Const
+    // Value must be known at compile-time, const int year  = 2022;
+    // Can't be changed after initialized.
+
+    // Final
+    // Value must be known at run-time, final birthday = getBirthDateFromDB()
+    // Can't be changed after initialized.
+
+    return HomePage();
   }
 }
