368390cd000881e3935ade0b6678f534912a0ab3
Tanisha <jaiswaltanisha11@gmail.com>
2023-04-17 17:26:19 +0530

    day8 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/drawer.dart';
+
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Tanisha";
 
   const HomePage({super.key});
-
-  //functions and parameters
-
-  // int count(int i) {
-  //   i++;
-  //   return i;
-  // }
-
-  // int counter({int i = 0}) {
-  //   i++;
-  //   return i;
-  // }
-
-  // int counters({ required bool isIncreased , int i = 1}) {
-  //   i++;
-  //   return i;
-  // }
-
  
   @override
   Widget build(BuildContext context) {
@@ -35,7 +19,7 @@ class HomePage extends StatelessWidget {
           child: Text("Welcome to $days days of flutter by $name"),
         ),
       ),
-      drawer: const Drawer(),
+      drawer: const MyDrawer(),
     );
   }
 }
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color:Colors.teal,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                accountName: Text("Tanisha Jaiswal"),
+                accountEmail: Text("jaiswaltanisha11@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  child: Image.asset("assets/images/womanpp.png"),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text("Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text("Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text("Email",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
