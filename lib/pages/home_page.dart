e6797946133559353378775f177494ad315f568b
Tanisha <jaiswaltanisha11@gmail.com>
2023-04-18 17:37:46 +0530

    day10 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
-
+import '../models/catalog.dart';
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
+
 
 class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "Tanisha";
-
   const HomePage({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of flutter by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index], 
+            );
+          },
         ),
       ),
       drawer: const MyDrawer(),
